c7ef99944ea9bf43eb0dca382e6801c7c5572825
Debra <dcooks@icstars.org>
2025-10-20 15:19:11 -0500

    debugging log

--- starter.js
+++ starter.js
@@ -3,18 +3,24 @@
 const numbers = [10, 20, 30, 40];
 
 
-let total1 = 0;
-for (let num of numbers) {
-  total1 += num;
-}
-console.log("Total 1:", total1);
 
-let total2 = 0;
+ let total =0;
+function calculateSum(){
+  
 for (let num of numbers) {
-  total2 += num;
+  total+= num;
 }
-console.log("Total 2:", total2);
 
+let result = console.log("Total 1:", total);
+return result
+}
+calculateSum();
 // Calculate the average
-let average = total / numbers.length;
-console.log("Average:", average);
+function calculateAverage (){
+  let average = total  / numbers.length;
+  let resultAverage =console.log("Average:", average);
+return resultAverage;
+
+
+}
+calculateAverage();
